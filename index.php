e2da55522c75dfbc384478a4a15d27ed90dee092
Amina Assaid <118275761+aminaassaid1@users.noreply.github.com>
2023-10-05 09:25:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ if (isset($_REQUEST['delete']) && $_REQUEST['delete'] == 'yes')
             <ul class="nav navbar-nav">
                 <li><a href="add.php"><span class="glyphicon glyphicon-plus"></span> Add New</a></li>
             </ul>
-        </div><!--/.nav-collapse -->
+        </div>
     </div>
 </nav>
 
